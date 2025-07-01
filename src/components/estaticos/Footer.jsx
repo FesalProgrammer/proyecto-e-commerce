aace6588ae7c30972e1df6f6992062a943724a0d
FesalProgrammer <FesalProgramming@gmail.com>
2025-07-01 16:59:27 -0300

    se amplia footer

--- src/components/estaticos/Footer.jsx
+++ src/components/estaticos/Footer.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import './styleEstatico.css'
+import React from "react";
+import "./styleEstatico.css";
+import flecha from "../../assets/flecha-hacia-arriba.png";
+
 const Footer = () => {
   return (
-    <footer>
-        <p>&copy; 2025 - Tienda Online</p>
+    <footer class="footer">
+      <nav class="navFooter">
+        <ul class="listaNav">
+          <li class="listaItem">
+            <a class="linkNavFooter" href="">
+              Términos y condiciones
+            </a>
+          </li>
+          <li class="listaItem">
+            <a class="linkNavFooter" href="">
+              Preguntas frecuentes
+            </a>
+          </li>
+          <li class="listaItem">
+            <a class="linkNavFooter" href="">
+              Ayuda
+            </a>
+          </li>
+          <li class="listaItem">
+            <a href="" class="flechaArriba">
+              <img src={flecha} alt="foto flecha" className="img-flecha" />
+            </a>
+          </li>
+        </ul>
+      </nav>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
